00d48b03c2bee0c74ccd4a4e53f74eff1a868c2e
Omri <omri.ronen4@gmail.com>
2021-07-22 15:53:00 -0700

    added home-routes

---
+++ controllers/home-routes.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const router = express.Router();
+const path = require('path');
+
+//Dashboard
+routes.get('/stats', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/stats.html'))
+});
+
+//New Workout 
+router.get('exercise', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/exercise.html'))
+});
+
+module.exports = router;
+
