7f8bf459eacded2fba654b919782157764b01253
Omri <omri.ronen4@gmail.com>
2021-07-21 17:32:43 -0700

    deploying

---
+++ server.js
@@ -0,0 +1,10 @@
+mongoose.connect(
+    process.env.MONGODB_URI || 'mongodb://localhost/deep-thoughts',
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+      useFindAndModify: false
+    }
+  );
+  
